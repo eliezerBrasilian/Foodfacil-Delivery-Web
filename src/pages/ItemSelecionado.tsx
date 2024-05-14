65756ddd73caff7c9c9738d380029dcbac66403e
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-05-14 09:16:34 -0300

    item selecionado

--- src/pages/ItemSelecionado.tsx
+++ src/pages/ItemSelecionado.tsx
@@ -14,28 +14,30 @@ export function ItemSelecionado() {
     if (id !== undefined) buscaItem(id);
   }, []);
 
-  if (id !== undefined)
-    return (
-      <div className={s.container}>
-        <img
-          className={s.seta_voltar}
-          src="/public/seta_voltar.png"
-          onClick={() => {
-            nav(-1);
-          }}
-        />
+  if (id !== undefined) {
+    if (buscaItem(id) !== undefined)
+      return (
+        <div className={s.container}>
+          <img
+            className={s.seta_voltar}
+            src="/seta_voltar.png"
+            onClick={() => {
+              nav(-1);
+            }}
+          />
 
-        <img
-          className={s.capa}
-          src={buscaItem(id)?.imagemRetangular}
-          height={200}
-          width={"100%"}
-        />
-        <p className={s.titulo}>{buscaItem(id)?.nome}</p>
-        <p className={s.descricao}>{buscaItem(id)?.descricao}</p>
-        <p className={s.preco}>{buscaItem(id)?.preco}</p>
-        <p className={s.taxa_de_entrega}>Taxa de entrega: R$2,00</p>
-      </div>
-    );
-  else return <p>Item não encontrado</p>;
+          <img
+            className={s.capa}
+            src={buscaItem(id)?.imagemRetangular}
+            height={200}
+            width={"100%"}
+          />
+          <p className={s.titulo}>{buscaItem(id)?.nome}</p>
+          <p className={s.descricao}>{buscaItem(id)?.descricao}</p>
+          <p className={s.preco}>{buscaItem(id)?.preco}</p>
+          <p className={s.taxa_de_entrega}>Taxa de entrega: R$2,00</p>
+        </div>
+      );
+    else return <p>Item não encontrado</p>;
+  } else return <p>Item não encontrado</p>;
 }
