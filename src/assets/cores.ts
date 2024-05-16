32dcdb21670e143ad8b8f28b257161a2ef2a8050
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-05-15 22:50:54 -0300

    item selecionado

--- src/assets/cores.ts
+++ src/assets/cores.ts
@@ -1,4 +1,5 @@
 export const cores = {
   font_ativa: "#FF0303",
   preco: "#28D84F",
+  btn_vermelho: "#FF0303",
 };
--- src/pages/ItemSelecionado.tsx
+++ src/pages/ItemSelecionado.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { cores } from "../assets/cores";
+import { Imagem } from "../components/Imagem";
 import { useSaborContext } from "../context/SaborContext";
 import { useSalgadosContext } from "../context/SalgadosContext";
 import s from "../modules/ItemSelecionado.module.css";
+import { AppUtils } from "../utils/AppUtils";
 import { ItemSelecionadoDetalhes } from "./ItemSelecionadoDetalhes";
 import { ItemSelecionadoTop } from "./ItemSelecionadoTop";
 import { MonteSeuPedido } from "./MonteSeuPedido";
@@ -15,6 +18,14 @@ export function ItemSelecionado() {
   const { lista: listaDeSabores } = useSaborContext();
 
   const [saboresEscolhidos, setSaboresEscolhidos] = useState<string[]>([]);
+
+  const [observacaoText, setObservacaoText] = useState("");
+
+  const onChangeText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (event.target.value.length < 141) {
+      setObservacaoText(event.target.value);
+    }
+  };
 
   const handleEscolheSabor = (sabor: string) => {
     if (id != null) {
@@ -46,8 +57,8 @@ export function ItemSelecionado() {
     }
   }, [salgados]);
 
-  console.log("-------escolhidos-------");
-  console.log(saboresEscolhidos);
+  // console.log("-------escolhidos-------");
+  // console.log(saboresEscolhidos);
 
   if (id !== undefined) {
     if (buscaItem(id) !== undefined)
@@ -65,8 +76,108 @@ export function ItemSelecionado() {
             saboresEscolhidos={saboresEscolhidos}
             onClick={handleEscolheSabor}
           />
+          <AlgumaObservacaoRow
+            contadorDeCaracteresDigitados={observacaoText.length}
+          />
+          <ObservacaoTextInput onChangeText={onChangeText} />
+          <ButaoAdicionarComPreco preco={buscaItem(id)?.preco} />
         </div>
       );
     else return <p>Item não encontrado</p>;
   } else return <p>Item não encontrado</p>;
+}
+
+export type Preco = number | string | undefined;
+
+interface ButaoAdicionarComPrecoProps {
+  preco: Preco;
+}
+
+export function ButaoAdicionarComPreco({ preco }: ButaoAdicionarComPrecoProps) {
+  return (
+    <div
+      style={{
+        backgroundColor: cores.btn_vermelho,
+        borderRadius: 10,
+        padding: 15,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        columnGap: 20,
+        width: "fit-content",
+        position: "absolute",
+        bottom: 20,
+        right: 20,
+        color: "#fff",
+      }}
+    >
+      <p>Adicionar</p>
+      <p>{AppUtils.toMoedaBrasileira(preco as number)}</p>
+    </div>
+  );
+}
+
+interface ObservacaoTextInputProps {
+  onChangeText: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+export function ObservacaoTextInput({
+  onChangeText,
+}: ObservacaoTextInputProps) {
+  return (
+    <div style={{ width: "100%", padding: 15 }}>
+      <textarea
+        onChange={onChangeText}
+        style={{
+          border: "1px solid #ccc",
+          borderRadius: "4px",
+          height: "140px",
+          padding: "8px",
+          resize: "none", // Impede o redimensionamento do textarea pelo usuário
+          width: "100%", // Ocupa toda a largura disponível,
+        }}
+        placeholder="Digite sua observação..."
+        maxLength={141}
+      />
+    </div>
+  );
+}
+
+interface AlgumaObservacaoRowProps {
+  contadorDeCaracteresDigitados?: number;
+}
+export function AlgumaObservacaoRow({
+  contadorDeCaracteresDigitados = 0,
+}: AlgumaObservacaoRowProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        paddingLeft: 15,
+        paddingRight: 15,
+      }}
+    >
+      <ObservacaoRowEsquerda />
+      <p style={{ color: "#4C4C4C", fontWeight: "600" }}>
+        {contadorDeCaracteresDigitados}/140
+      </p>
+    </div>
+  );
+}
+
+function ObservacaoRowEsquerda() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        columnGap: 10,
+      }}
+    >
+      <Imagem imagePath="/public/message.png" height={26} width={26} />
+      <p style={{ color: "#4C4C4C", fontWeight: "600" }}>Alguma observação?</p>
+    </div>
+  );
 }
