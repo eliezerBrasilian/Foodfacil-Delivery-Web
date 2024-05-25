19176ed75f7553f2ab3efd3dd4cfe218c0f1bcf3
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-05-25 19:40:41 -0300

    tela pedidos

--- src/pages/TelaPedidos.tsx
+++ src/pages/TelaPedidos.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { AppTitulo } from "../components/AppTitulo";
+import { CustomBtn } from "../components/CustomBtn";
 import { CustomLoading } from "../components/CustomLoading";
+import { Imagem } from "../components/Imagem";
 import { PedidoItem } from "../components/PedidoItem";
 import { useBottomBarContext } from "../context/BottomBarContext";
 import { usePedidoContext } from "../context/PedidoContext";
+import { Rotas } from "../enums/Rotas";
 import s from "../modules/TelaPedido.module.css";
 import { PedidoDoUsuarioResponseDto } from "../types/PedidoDoUsuarioResponseDto";
 
@@ -28,13 +32,13 @@ export function TelaPedidos() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const nav = useNavigate();
+
   return (
     <div className={s.container}>
       <AppTitulo text="Meus Pedidos" />
 
       <div style={{ padding: 15 }}>
-        <h3>Histórico</h3>
-
         {carregando ? (
           <div
             style={{
@@ -46,7 +50,12 @@ export function TelaPedidos() {
             <CustomLoading />
           </div>
         ) : (
-          <PedidoMainContent pedidos={pedidos} />
+          <PedidoMainContent
+            pedidos={pedidos}
+            verCardapioClick={() => {
+              nav(Rotas.TELA_CARDAPIO);
+            }}
+          />
         )}
       </div>
     </div>
@@ -55,12 +64,36 @@ export function TelaPedidos() {
 
 interface PedidoMainContentprops {
   pedidos: PedidoDoUsuarioResponseDto[];
+  verCardapioClick: () => void;
 }
 
-export function PedidoMainContent({ pedidos }: PedidoMainContentprops) {
-  if (pedidos.length == 0) return <p>Sem pedidos no momento</p>;
+export function PedidoMainContent({
+  pedidos,
+  verCardapioClick,
+}: PedidoMainContentprops) {
+  if (pedidos.length == 0)
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          rowGap: 10,
+          marginTop: 120,
+        }}
+      >
+        <Imagem height={160} width={160} imagePath="/sem_pedidos_image.png" />
+        <h3>Você ainda não realizou um pedido!</h3>
+        <p style={{ textAlign: "center" }}>
+          Bora Pedir? Acesse o cardápio para ver as opções disponíveis
+        </p>
+        <CustomBtn text="Ver cardápio" width="90%" onClick={verCardapioClick} />
+      </div>
+    );
   return (
-    <div style={{ marginBottom: 70, marginTop: 10 }}>
+    <div style={{ marginBottom: 60 }}>
+      <h3 style={{ marginBottom: 10 }}>Histórico</h3>
       {pedidos.map((v, i) => (
         <PedidoItem pedido={v} key={i} />
       ))}
